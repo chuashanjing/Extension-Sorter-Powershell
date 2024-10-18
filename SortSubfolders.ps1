4f5536d1d1b856159578c1617644ef3228102900
sj <151940896+chuashanjing@users.noreply.github.com>
2024-10-19 01:07:47 +0800

    Add files via upload

---
+++ SortSubfolders.ps1
@@ -0,0 +1,18 @@
+﻿# Define the main folder path
+$mainFolder = "<main folder>"
+
+# Destination path for PDF files
+$destinationFolder = "<destination>"
+
+# Ensure the destination folder exists
+if (!(Test-Path $destinationFolder)) {
+    New-Item -ItemType Directory -Path $destinationFolder
+}
+
+# Move only PDF files from subfolders
+Get-ChildItem -Path $mainFolder -Recurse -File -Filter *.pdf | ForEach-Object {
+    if($_.Name -notlike "*SHEET*"){
+        Copy-Item -LiteralPath $_.FullName -Destination $destinationFolder
+    }
+}
+
